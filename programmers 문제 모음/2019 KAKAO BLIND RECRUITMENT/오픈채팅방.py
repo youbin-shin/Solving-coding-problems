ba73dad4b716e98c359635f9756cc06708cfce35
신유빈 <youbin1294@naver.com>
2020-09-05 16:47:31 +0900

    0905 | programmers | 2019 +1

---
+++ programmers 문제 모음/2019 KAKAO BLIND RECRUITMENT/오픈채팅방.py
@@ -0,0 +1,20 @@
+def solution(record):
+    answer = []
+    user_info = {}
+
+    for i in range(len(record)-1, -1, -1): # record list 뒤부터 확인하기
+        r = list(map(str, record[i].split()))
+        if r[0] == "Enter" or r[0] == "Change": # uid를 key로 name을 value로 저장하기
+            if r[1] not in user_info.keys():
+                user_info[r[1]] = r[2]
+
+    # 원하는 메시지로 저장하기
+    for j in range(len(record)):
+        r = list(map(str, record[j].split()))
+        if r[0] == "Enter":
+            temp = user_info.get(r[1]) + "님이 들어왔습니다."
+            answer.append(temp)
+        elif r[0] == "Leave":
+            temp = user_info.get(r[1]) + "님이 나갔습니다."
+            answer.append(temp)
+    return answer
