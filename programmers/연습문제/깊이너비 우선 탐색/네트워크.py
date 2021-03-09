d13a6d3e91548652502d1dfc3194e605f6fbc426
youbin-shin <youbin5575@gmail.com>
2021-03-09 09:00:35 +0900

    programmers | 연습문제 | 깊이너비 우선 탐색 | 네트워크

--- programmers/연습문제/깊이너비 우선 탐색/네트워크.py
+++ programmers/연습문제/깊이너비 우선 탐색/네트워크.py
@@ -1,22 +1,28 @@
+from collections import deque
+
 def solution(n, computers):
-    network = []
-    visited = [0] * n
-    for i in range(n):
-        if visited[i] == 0:
-            visited[i] = 1
-            q = [i]
-            temp = [i] # 하나의 네트워크를 이루는 컴퓨터를 기록할 리스트
-            while q:
-                idx = q.pop(0)
-                for j in range(n):
-                    if computers[idx][j] == 1 and visited[j] == 0:
-                        q.append(j)
-                        temp.append(j)
-                        visited[j] = 1
-            network.append(temp)
-    return len(network)
+    visited = [False] * n # 네트워크에 연결됐는지 확인하는 리스트
+    q = deque([0])
+    k = 1 # 네트워크에 포함된 컴퓨터 수 저장 변수
+    answer = 1 # 네트워크 개수 저장하는 변수
+    while k <= n: # 컴퓨터가 네트워크에 모두 포함될 때까지
+        if len(q) == 0: # 네트워크 추가 필요!!
+            answer += 1
+            for v in range(n):
+                # 아직 네트워크 연결안된 컴퓨터 하나 찾아서 q에 삽입
+                if visited[v] == False:
+                    q.append(v)
+                    break
+        # BFS를 이용하여 연결된 네트워크 탐색하기
+        c = q.popleft()
+        for i in range(n):
+            if computers[c][i] == 1 and visited[i] == False:
+                q.append(i)
+                visited[i] = True
+                k += 1
+    return answer
 
 
-n = 3
-computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
-print(solution(n, computers))
+# n = 3
+# computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
+# print(solution(n, computers))
