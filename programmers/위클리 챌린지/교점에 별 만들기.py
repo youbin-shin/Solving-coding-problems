d4534732ab1cfa884365b9d5527ba5d45c5711b9
shin youbin <youbin5575@gmail.com>
2022-01-12 23:38:51 +0900

    Add programmers problem solving

--- programmers/위클리 챌린지/교점에 별 만들기.py
+++ programmers/위클리 챌린지/교점에 별 만들기.py
@@ -1,5 +1,4 @@
 def solution(line):
-    answer = []
     # 1. 교점 찾기
     line_cnt = len(line)
     point_list = []
@@ -22,21 +21,19 @@ def solution(line):
                 min_y = min(min_y, y)
                 max_y = max(max_y, y)
                 point_list.append([x, y])
-    print(point_list)
+
     x_length, y_length = 1, 1
     if min_x != max_x:
-        x_length = max_x - min_x
+        x_length = max_x - min_x + 1
     if min_y != max_y:
-        y_length = max_y - min_y
+        y_length = max_y - min_y + 1
+
     answer = ["." * x_length for _ in range(y_length)]
-    zero_x, zero_y = (max_x + min_x) // 2, (max_y + min_y) // 2
-    print(zero_x, zero_y)
-    for px, py in point_list:
-        print(x_length + px, y_length + py, answer[zero_x + px])
-        # answer[][] = "*"
-    print(answer)
+    # 2. 좌표에 * 찍기
+    for x, y in point_list:
+        answer[max_y - y] = (
+            answer[max_y - y][: x - min_x] + "*" + answer[max_y - y][x - min_x + 1 :]
+        )
+    for a in range(len(answer)):
+        answer[a] = "".join(answer[a])
     return answer
-
-
-line = [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]
-print(solution(line))
