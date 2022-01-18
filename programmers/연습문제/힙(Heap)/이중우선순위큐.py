b9b25e1c0d88f1b99fc840c4f3a4abce1d946c21
shin youbin <youbin5575@gmail.com>
2022-01-18 23:10:13 +0900

    Add programmers problem solving

--- programmers/연습문제/힙(Heap)/이중우선순위큐.py
+++ programmers/연습문제/힙(Heap)/이중우선순위큐.py
@@ -1,3 +1,4 @@
+# solution 1) heapq 활용
 import heapq
 
 def solution(operations):
@@ -16,4 +17,22 @@ def solution(operations):
     if hq == []: answer = [0, 0]
     else:
         answer = [heapq.nlargest(1, hq)[0], heapq.nsmallest(1, hq)[0]]
+    return answer
+
+# solution 2) 리스트 활용
+def solution(operations):
+    answer = []
+    for op in operations:
+        op1, op2 = op.split(" ")
+        op2 = int(op2)
+        if op1 == "I":
+            answer.append(op2)
+        elif answer != []: # op1 = "D"
+            if op2 == 1:
+                answer.pop(answer.index(max(answer)))
+            else:
+                answer.pop(answer.index(min(answer)))
+    if answer == []: answer = [0, 0]
+    else:
+        answer = [max(answer), min(answer)]
     return answer
