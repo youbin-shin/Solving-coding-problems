b1ae653b0788e7e02ef584fcaf0b1f7b806d3beb
youbin-shin <youbin5575@gmail.com>
2022-02-22 21:42:20 +0900

    Add programmers problem solving

---
+++ programmers/Summer Winter Coding(2019)/우유와 요거트가 담긴 장바구니.sql
@@ -0,0 +1,11 @@
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE (
+    CART_ID IN (
+        SELECT CART_ID
+        FROM CART_PRODUCTS
+        WHERE NAME = "Milk"
+    )
+    AND NAME = 'Yogurt'
+)
+ORDER BY CART_ID;
