edb446f5899b054463d90329d8c9522660a6661b
youbin-shin <youbin5575@gmail.com>
2022-03-22 01:21:45 +0900

    Add programmers problem solving

--- programmers/2021 KAKAO BLIND RECRUITMENT/순위 검색.py
+++ programmers/2021 KAKAO BLIND RECRUITMENT/순위 검색.py
@@ -1,3 +1,53 @@
+from itertools import combinations
+
+
+def solution(info, query):
+    answer = []
+
+    info_dict = dict()
+    for i in info: # i = "java backend junior pizza 150"
+        i_list = i.split(" ")
+        i_key = i_list[:-1] # ['java', 'backend', 'junior', 'pizza']
+        i_value = i_list[-1] # 150
+
+        # i_key로 만들 수 있는 모든 경우 info_dict에 저장
+        for num in range(5):
+            for c in combinations(i_key, num):
+                key = "".join(c)
+                if key in info_dict:
+                    info_dict[key].append(int(i_value))
+                else:
+                    info_dict[key] = [int(i_value)]
+
+    for id in info_dict:
+        info_dict[id].sort()
+
+    for q in query:
+        q_list = q.split(" ")
+        while "and" in q_list:
+            q_list.remove("and")
+        while "-" in q_list:
+            q_list.remove("-")
+        q_key = "".join(q_list[:-1])
+        q_value = int(q_list[-1])
+
+        res = 0
+        if q_key in info_dict: # 효율성을 위해 필요
+            res_list = info_dict[q_key]
+            if len(res_list) > 0:
+                left, right = 0, len(res_list)
+                while left < right:
+                    mid = (left + right) // 2
+                    if res_list[mid] >= q_value:
+                        right = mid
+                    else:
+                        left = mid + 1
+                res = len(res_list) - left
+
+        answer.append(res)
+    return answer
+
+
 # 정확성은 통과이지만 효율성은 실패하는 코드
 def solution(info, query):
     answer = []
